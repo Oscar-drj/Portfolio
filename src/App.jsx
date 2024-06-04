3d74b768d4b5bade3b139505315c33a64059ac68
OscarDelRey <oscardelreyjuarez@gmail.com>
2024-06-04 12:44:15 +0200

    First project commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Nav } from './components/Nav'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <Nav />
+      </header>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,10 @@
+export function Nav() {
+    return (
+        <div className="nav-bar">
+            <a href="">Sobre mi</a>
+            <a href="">Proyectos</a>
+            <a href="">Experiencia</a>
+            <a href="">Contacto</a>
+        </div>
+    )
+}
